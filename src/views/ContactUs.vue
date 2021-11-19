1cba3740febb4d7c308891e030f94b44dbf91e75
Maria K <2020mariaku@gmail.com>
2021-11-19 16:05:59 +0200

    Unfinished Contact Us Page

---
+++ src/views/ContactUs.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="contactUs">
+		<div>
+			<h1>
+				Feel free to contact our team by any of the following means:
+			</h1>
+			<p> info </p>
+		</div>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped lang="scss">
+h1 {
+	margin-bottom: 100px;
+}
+
+ul {
+	list-style: none;
+	display: flex;
+	justify-content: space-around;
+	flex-flow: row wrap;
+	padding: 0;
+}
+</style>
