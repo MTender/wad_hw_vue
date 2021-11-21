c20fb45ce2daa1251701ca2079586b204ca82013
sander <sandersoodla@gmail.com>
2021-11-21 14:22:02 +0200

    contact info

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -4,7 +4,16 @@
 			<h1>
 				Feel free to contact our team by any of the following means:
 			</h1>
-			<p> info </p>
+			<ul>
+				<li>
+					<h2>Telephone:</h2> 
+					<p>+37265656565</p>
+				</li>
+				<li>
+					<h2>Email:</h2> 
+					<p>contact@postit.com</p>
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
